e21e11c9834dd7db9db4dabf918e4fd63b3e89a4
Ruby Rose <deadsurgeon42@gmail.com>
2018-07-07 19:52:26 +0300

    Update AssemblyInfo.cs

--- SpawnMobX/Properties/AssemblyInfo.cs
+++ SpawnMobX/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpawnMobX")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Spawn mobs with special properties!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpawnMobX")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
